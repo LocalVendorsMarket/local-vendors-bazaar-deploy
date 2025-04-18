15863f135fd4935f5b11da3838e9c4ea1f2234a6
Waheed Hussain <waheedhussain@Waheeds-MacBook-Pro.local>
2025-04-18 17:03:34 -0500

    Added temporary Faq.jsx page to fix build

---
+++ client/src/pages/Faq.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Faq = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-600">
+        FAQ Page Coming Soon!
+      </h1>
+    </div>
+  );
+};
+
+export default Faq;
