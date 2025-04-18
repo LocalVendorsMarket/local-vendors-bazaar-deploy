89ba71109056a7be5c5737ee1a54bc9f09b10481
Waheed Hussain <waheedhussain@Waheeds-MacBook-Pro.local>
2025-04-18 16:22:26 -0500

    Fixed App.js duplicate imports and cleaned routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Blog from './Blog';
-import Faq from './Faq';
-import Contact from './Contact';
-import VendorSignupPage from './VendorSignup';
-import ThankYou from './ThankYou'; // ✅ import thank-you page
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Faq from './pages/Faq';
 import Contact from './pages/Contact';
 import VendorSignupPage from './pages/VendorSignupPage';
 import ThankYou from './pages/ThankYouPage';
-import ShopPage from './pages/ShopPage'; // ✅ <-- NEW IMPORT for ShopPage
+import ShopPage from './pages/ShopPage'; // ✅ NEW SHOPPAGE IMPORT
 
 function App() {
   return (
@@ -26,13 +18,14 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/signup" element={<VendorSignupPage />} />
         <Route path="/thank-you" element={<ThankYou />} />
-        <Route path="/shop" element={<ShopPage />} /> {/* ✅ NEW SHOP ROUTE */}
+        <Route path="/shop" element={<ShopPage />} /> {/* ✅ SHOP PAGE */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
