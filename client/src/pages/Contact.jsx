18d246626bb2aac057db5304f34197aeeb644c5e
Waheed Hussain <waheedhussain@Waheeds-MacBook-Pro.local>
2025-04-18 17:08:19 -0500

    Added temporary Contact.jsx page to fix build

---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-600">
+        Contact Page Coming Soon!
+      </h1>
+    </div>
+  );
+};
+
+export default Contact;
