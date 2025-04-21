7171da51772cf1dc318033ba3902ceff25b0cf95
Waheed Hussain <waheedhussain@Waheeds-MacBook-Pro.local>
2025-04-20 22:30:43 -0500

    Fixed App.js import Home correctly

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home'; // ✅ This was missing
 import Blog from './pages/Blog';
 import Faq from './pages/Faq';
 import Contact from './pages/Contact';
