5f70fa5678b5fc288995600e90d99dd0dce83f31
Waheed Hussain <waheedhussain@Waheeds-MacBook-Pro.local>
2025-06-07 00:47:06 -0500

    Fix: Correct invalid ShopPage import in components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ShopPage from './pages/ShopPage';
+import ShopPage from '../pages/ShopPage';
 import VendorLoginPage from './pages/VendorLoginPage';
 import VendorDashboard from './pages/VendorDashboard';
 import ProductManagement from './pages/ProductManagement';
