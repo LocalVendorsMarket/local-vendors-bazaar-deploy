abba73b84df2832e58b53d639ec21249b36af779
Waheed Hussain <waheedhussain@Waheeds-MacBook-Pro.local>
2025-06-13 17:01:33 -0500

    Fix: Removed duplicate Router from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
 import VendorLoginPage from './pages/VendorLoginPage';
@@ -14,65 +14,64 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/vendor-login" element={<VendorLoginPage />} />
-        <Route
-          path="/vendor-dashboard"
-          element={
-            <ProtectedRoute>
-              <VendorDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/vendor-products"
-          element={
-            <ProtectedRoute>
-              <ProductManagement />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/vendor-orders"
-          element={
-            <ProtectedRoute>
-              <OrderManagement />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/vendor-analytics"
-          element={
-            <ProtectedRoute>
-              <AnalyticsReports />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/vendor-account"
-          element={
-            <ProtectedRoute>
-              <AccountSettings />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/vendor-support"
-          element={
-            <ProtectedRoute>
-              <VendorSupport />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/shop" element={<ShopPage />} />
+      <Route path="/vendor-login" element={<VendorLoginPage />} />
+      <Route
+        path="/vendor-dashboard"
+        element={
+          <ProtectedRoute>
+            <VendorDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/vendor-products"
+        element={
+          <ProtectedRoute>
+            <ProductManagement />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/vendor-orders"
+        element={
+          <ProtectedRoute>
+            <OrderManagement />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/vendor-analytics"
+        element={
+          <ProtectedRoute>
+            <AnalyticsReports />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/vendor-account"
+        element={
+          <ProtectedRoute>
+            <AccountSettings />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/vendor-support"
+        element={
+          <ProtectedRoute>
+            <VendorSupport />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
 export default App;
+
 
 
 
